00e13ee07098693f0e6f87925f2af8de78c11021
Pier-Luc Bonneville <pier-luc.bonneville@priv.gc.ca>
2020-08-21 09:48:58 -0400

    Added rules

--- ComplaintFormCore/ClientApp/.eslintrc.js
+++ ComplaintFormCore/ClientApp/.eslintrc.js
@@ -18,5 +18,15 @@ module.exports = {
         "eslint:recommended",
         "plugin:@typescript-eslint/recommended",
         "plugin:@typescript-eslint/recommended-requiring-type-checking"
-    ]
+    ],
+    rules: {
+        "@typescript-eslint/no-unsafe-call": 1,
+        "@typescript-eslint/no-unsafe-member-access": 1,
+        // "max-lines-per-function": ["error", {
+        //     "max": 30,
+        //     "skipBlankLines": true,
+        //     "skipComments": true
+        // }]
+        "max-statements": ["error", { "max": 10 }]
+    }
 };
