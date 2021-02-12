5fc5ffb8bf516f09a27726e20411f2e015110a7b
Jean-Francois Brouillette <Jean-Francois.Brouillette@priv.gc.ca>
2021-02-12 13:45:35 -0500

    Fixed broken build post merge

--- ComplaintFormCore/Web Apis/ContactInfoSurveyController.cs
+++ ComplaintFormCore/Web Apis/ContactInfoSurveyController.cs
@@ -4,7 +4,6 @@ using System.IO;
 using System.Linq;
 using ComplaintFormCore.Exceptions;
 using ComplaintFormCore.Models;
-using ComplaintFormCore.Models.pa;
 using ComplaintFormCore.Resources;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Localization;
